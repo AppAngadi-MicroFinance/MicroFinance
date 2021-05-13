5bcce73798692fd1eae20c28dd0ea8b08a0bc3d7
Ashraf ali <examsign.ashraf@outlook.com>
2021-05-13 15:43:47 +0530

    CreateBranchPage

---
+++ MicroFinance/MicroFinance/CreateBranch.xaml.cs
@@ -0,0 +1,62 @@
+﻿using MicroFinance.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicroFinance
+{
+    /// <summary>
+    /// Interaction logic for CreateBranch.xaml
+    /// </summary>
+    public partial class CreateBranch : Page
+    {
+        public List<string> Regionlist =new List<string>() {"Chennai", "Trichy", "Coimbatore", "Salem" };
+        public List<string> Managerlist = new List<string>() { "Ashraf", "SAfdhar", "Thalif", "Santhosh" };
+        public List<string> Accountantlist = new List<string>() { "Ashraf", "SAfdhar", "Thalif", "Santhosh" };
+        Createbranch CB = new Createbranch();
+        public CreateBranch()
+        {
+            InitializeComponent();
+            MainGrid.DataContext = CB;
+            RegionBox.ItemsSource = Regionlist;
+            Managerbox.ItemsSource = Managerlist;
+            Accountantbox.ItemsSource = Accountantlist;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BranchAccountdetailsPanel.IsOpen = true;
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            BranchAccountdetailsPanel.IsOpen = false;
+        }
+
+        private void Savebtn_Click(object sender, RoutedEventArgs e)
+        {
+            ConfirmPanel.IsOpen = true;
+        }
+
+        private void Cancelbtn_Click(object sender, RoutedEventArgs e)
+        {
+            ConfirmPanel.IsOpen = false;
+        }
+
+        private void PanelCloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BranchAccountdetailsPanel.IsOpen = false;
+        }
+    }
+}
---
+++ MicroFinance/MicroFinance/Modal/Createbranch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroFinance.Modal
+{
+    class Createbranch
+    {
+        public Createbranch()
+        {
+            this._costpermonth = 0;
+        }
+        public string RegionName { get; set; }
+        public string BranchName { get; set; }
+        public string BranchAddress { get; set; }
+        public string LandlineNumber { get; set; }
+        int _costpermonth;
+        public string CostPerMonth
+        {
+            get
+            {
+                return _costpermonth.ToString();
+            }
+            set
+            {
+                _costpermonth = int.Parse(value);
+            } 
+        }
+        public string ManagerName { get; set; }
+        public string AccountantName { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public string EBConnectionName { get; set; }
+        public string EBConnectionNumber { get; set; }
+        public string InternetConnectionName { get; set; }
+        public int InternetConnectionCost { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerContactNumber { get; set; }
+        public string OwnerAddress { get; set; }
+        public int AdvancePaid { get; set; }
+        public int RentPerMonth { get; set; }
+        public string AccountHolderName { get; set; }
+        public string AccountNumber { get; set; }
+        public string BankName { get; set; }
+        public string BankBranchName { get; set; }
+        public string IFSCCode { get; set; }
+        public string MICRCODE { get; set; }
+
+    }
+}
